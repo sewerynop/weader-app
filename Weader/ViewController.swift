218a28cf5da6c164af4f1c78da941665dcc07a35
Seweryn <sewerynop@gmail.com>
2017-09-02 19:01:35 +0200

    Znaki-w-j_polskim
    Zmiany wprowadzone przez Jacka 02-09-2017

--- Weader/ViewController.swift
+++ Weader/ViewController.swift
@@ -21,79 +21,102 @@ class ViewController: UIViewController, UISearchBarDelegate {
     var condition: String!
     var imgURL: String!
     var city: String!
-
+    
     var exists: Bool = true
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         searchBar.delegate = self
-       
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func urlWithComponents(cityName: String) -> URL? {
+        
+        let urlComponents = NSURLComponents()
+        urlComponents.scheme = "http";
+        urlComponents.host = "api.apixu.com"
+        urlComponents.path = "/v1/current.json"
+        
+        let key = URLQueryItem(name: "key", value: "379d278a34c84eac97e182514172801")
+        let city = URLQueryItem(name: "q", value: cityName)
+        urlComponents.queryItems = [key, city]
+        
+        return urlComponents.url
+    }
+    
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let urlRequest = URLRequest(url: URL(string: "http://api.apixu.com/v1/current.json?key=379d278a34c84eac97e182514172801&q=\(searchBar.text!.replacingOccurrences(of: "", with: "%20"))")!)
         
-        
-    let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-        
-        if error == nil {
-            do {
-                let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
-                
-                if let current = json["current"] as? [String: AnyObject] {
-                    
-                    if let temp = current["temp_c"] as? Int {
-                    self.degree = temp
-                    }
-                    if let condition = current["condition"] as? [String: AnyObject] {
-                        self.condition = condition["text"] as! String
-                        let icon = condition["icon"] as! String
-                        self.imgURL = "http:\(icon)"
-                    }
-                }
-                if let location = json["location"] as? [String: AnyObject] {
-                    self.city = location["name"] as! String
-                }
-                if let _ = json["error"] {
-                    self.exists = false
-                }
-            
-                DispatchQueue.main.async {
-                    if self.exists {
-                        
-                        self.degreeLabel.isHidden = false
-                        self.contitionLabel.isHidden = false
-                        self.imageView.isHidden = false
-                        
-                        self.degreeLabel.text = "\(self.degree.description)°"
-                        self.cityLabel.text = self.city
-                        self.contitionLabel.text = self.condition
-                        self.imageView.downloadImage(from: self.imgURL!)
-                    }else{
-                        self.degreeLabel.isHidden = true
-                        self.contitionLabel.isHidden = true
-                        self.imageView.isHidden = true
-                        self.cityLabel.text = "Nie znaleziono takiego miasta"
-                        self.exists = true
-                    }
-                }
-                
-            }catch let jsonError {
-                print(jsonError.localizedDescription)
-                
-            }
+        guard searchBar.text != nil else {
+            return
         }
+        
+        if let url = urlWithComponents(cityName: searchBar.text!) {
+            let urlRequest = URLRequest(url: url)
+            
+            let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+                
+                if error == nil {
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String : AnyObject]
+                        
+                        if let current = json["current"] as? [String: AnyObject] {
+                            
+                            if let temp = current["temp_c"] as? Int {
+                                self.degree = temp
+                            }
+                            if let condition = current["condition"] as? [String: AnyObject] {
+                                self.condition = condition["text"] as! String
+                                let icon = condition["icon"] as! String
+                                self.imgURL = "http:\(icon)"
+                            }
+                        }
+                        if let location = json["location"] as? [String: AnyObject] {
+                            self.city = location["name"] as! String
+                        }
+                        if let _ = json["error"] {
+                            self.exists = false
+                        }
+                        
+                        DispatchQueue.main.async {
+                            if self.exists {
+                                
+                                self.degreeLabel.isHidden = false
+                                self.contitionLabel.isHidden = false
+                                self.imageView.isHidden = false
+                                
+                                self.degreeLabel.text = "\(self.degree.description)°"
+                                self.cityLabel.text = self.city
+                                self.contitionLabel.text = self.condition
+                                self.imageView.downloadImage(from: self.imgURL!)
+                            }else{
+                                self.degreeLabel.isHidden = true
+                                self.contitionLabel.isHidden = true
+                                self.imageView.isHidden = true
+                                self.cityLabel.text = "Nie znaleziono takiego miasta"
+                                self.exists = true
+                            }
+                        }
+                        
+                    }catch let jsonError {
+                        print(jsonError.localizedDescription)
+                        
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+        
     }
-        task.resume()
-    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
@@ -101,21 +124,17 @@ class ViewController: UIViewController, UISearchBarDelegate {
 }
 
 
-    extension UIImageView {
-        func downloadImage(from url: String) {
-            let urlRequest = URLRequest(url: URL(string: url)!)
-            
-                let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-                    if error == nil {
-                        DispatchQueue.main.sync {
-                            self.image = UIImage(data: data!)
-                        }
-                    }
+extension UIImageView {
+    func downloadImage(from url: String) {
+        let urlRequest = URLRequest(url: URL(string: url)!)
+        
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if error == nil {
+                DispatchQueue.main.sync {
+                    self.image = UIImage(data: data!)
+                }
             }
-            task.resume()
         }
+        task.resume()
+    }
 }
-    
-
-
-
